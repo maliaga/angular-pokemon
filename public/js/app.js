786850870ca03f6523e9f25a5083d691bd7cb9f8
Mario Aliaga <ma@marioaliaga.com>
2017-03-02 22:24:42 -0300

    Agrego imagenes, filtros y controladores

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,27 @@
 (function () {
+
     var app = angular.module('pokedex', [
+        'ngRoute',
         'pokedex.controllers',
-        'pokedex.filters',
-        'pokedex.directives'
+        'pokedex.directives',
+        'pokedex.filters'
     ]);
+
+    app.config(['$routeProvider', function ($routeProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/pokedex.html',
+                controller: 'PokedexController'
+            })
+            .when('/pokemon/:id', {
+                templateUrl: 'views/pokemon.html',
+                controller: 'PokemonController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+    }]);
+
 })();
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,70 +1,68 @@
-/**
- * Created by maliaga on 3/2/17.
- */
 (function () {
-    angular.module('pokedex.directives',[])
-        .directive('pokemonData', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-data.html'
-        }
-    })
-        .directive('pokemonEvolution', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-evolution.html'
-        }
-    })
-        .directive('pokemonImage', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-image.html'
-        }
-    })
+
+    angular.module('pokedex.directives', [])
         .directive('pokemonName', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-name.html'
-        }
-    })
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/pokemon-name.html'
+            };
+        })
+
+        .directive('pokemonImage', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/pokemon-image.html'
+            };
+        })
+
+        .directive('pokemonData', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/pokemon-data.html'
+            };
+        })
+
         .directive('pokemonStats', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-stats.html'
-        }
-    })
-        .directive('pokemonNav', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-nav.html'
-        }
-    })
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/pokemon-stats.html'
+            };
+        })
+
+        .directive('pokemonEvolution', function () {
+            return {
+                retrict: 'E',
+                templateUrl: 'partials/pokemon-evolution.html'
+            };
+        })
+
         .directive('pokemonComments', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'partials/pokemon-comments.html',
-            controller : function () {
-                this.comments = [];
-                this.comment = {};
-                this.show = false;
+            return {
+                restrict: 'E',
+                templateUrl: 'partials/pokemon-comments.html',
+                controller: function () {
+                    this.comments = [];
+                    this.comment = {};
+                    this.show = false;
 
-                this.toggle = function () {
-                    this.show = !this.show;
-                };
+                    this.toggle = function () {
+                        this.show = !this.show;
+                    };
 
-                this.anonymousChanged = function () {
-                    if (this.comment.anonymous) {
-                        this.comment.email = "";
-                    }
-                };
+                    this.anonymousChanged = function () {
+                        if (this.comment.anonymous) {
+                            this.comment.email = "";
+                        }
+                    };
 
-                this.addComment = function () {
-                    this.comment.date = Date.now();
-                    this.comments.push(this.comment);
-                    this.comment = [];
-                }
-            },
-            controllerAs : 'cmtsCtrl'
-        }
-    });
+                    this.addComment = function () {
+                        this.comment.date = Date.now();
+                        this.comments.push(this.comment);
+                        this.comment = {};
+                    };
+                },
+                controllerAs: 'cmtsCtrl'
+            };
+        });
+
 })();
